e4e260b562e6b79a3b6a091e5b76da10861eec3c
Jesus Alonso <alonsojesus2003@gmail.com>
2026-01-08 01:39:11 -0600

    add route flower

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan';
 import {PORT} from './config.js'
+import FlowersRoutes from './routes/flowers.routes.js'
 
 
 const app = express();
@@ -12,13 +13,11 @@ app.use(express.json());
 app.use((err, req, res, next) =>{
     return res.status(500).json({message : err.message})
 });
+app.use(FlowersRoutes);
 
 app.get('/', (req, res)=>{
     return res.send('Bienvenido a la florería')
 })
-
-
-
 
 
 app.listen(PORT);
