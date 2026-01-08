5627f0e95a1c5cdf4dcacd7c51b17132ef9d483d
Jesus Alonso <alonsojesus2003@gmail.com>
2026-01-07 23:47:57 -0600

    Cambio en error handler

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import morgan from 'morgan';
+import {PORT} from './config.js'
+
+
+const app = express();
+
+app.use(express());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((err, req, res, next) =>{
+    return res.status(500).json({message : err.message})
+});
+
+app.get('/', (req, res)=>{
+    return res.send('Bienvenido a la florería')
+})
+
+
+
+
+
+app.listen(PORT);
+
+console.log(`Servidor corriendo en http://localhost:${PORT}`);
