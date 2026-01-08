fc93073a60182280dfbd44694985429f060fb637
Jesus Alonso <alonsojesus2003@gmail.com>
2026-01-08 01:03:40 -0600

    configurate db conection

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import pg from 'pg';
+import {DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT} from './config.js';
+
+export const pool = new pg.Pool({
+    user: DB_USER,         
+    password: DB_PASS,
+    host: DB_HOST,         
+    port: DB_PORT,         
+    database: DB_NAME   
+});
