6eb957dfce9c1af6ca8411ebe4bdc34b6979b411
Jesus Alonso <alonsojesus2003@gmail.com>
2026-01-08 19:33:07 -0600

    update in endpoints

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import morgan from 'morgan';
 import {PORT} from './config.js'
 import FlowersRoutes from './routes/flowers.routes.js'
-
+import Credentials from './routes/login.routes.js';
 
 const app = express();
 
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use((err, req, res, next) =>{
     return res.status(500).json({message : err.message})
 });
-app.use(FlowersRoutes);
+app.use('/api', FlowersRoutes, Credentials);
 
-app.get('/', (req, res)=>{
+app.get('/api', (req, res)=>{
     return res.send('Bienvenido a la florería')
 })
 
