465de323e73acc987f86906c2dc7454eba141427
Jesus Alonso <alonsojesus2003@gmail.com>
2026-01-08 19:30:32 -0600

    no changes

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import pg from 'pg';
 import {DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT} from './config.js';
  
-const {Pool} = pg;
 
-export const pool = new Pool({
+
+export const pool = new pg.Pool({
     user: DB_USER,         
     password: DB_PASS,
     host: DB_HOST,         
