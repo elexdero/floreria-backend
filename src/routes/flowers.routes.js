a805d77be301d4eaf460acd222d4c6e38c08e27a
Jesus Alonso <alonsojesus2003@gmail.com>
2026-01-08 19:28:11 -0600

    modified routes

--- src/routes/flowers.routes.js
+++ src/routes/flowers.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { getFlowers } from "../controllers/flowers.controller.js";
+import { getFlowers, getFlowerById, postFlower } from "../controllers/flowers.controller.js";
 
 const router = Router();
 
 router.get('/flowers', getFlowers);
+router.get('/flowers/:id', getFlowerById);
+router.post('/flowers', postFlower);
 
 
 export default router;
